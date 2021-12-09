be4876827c1fb9ee36c224fd04d81fba0c767e08
Krishna Bisht <kb22880@gmail.com>
2021-12-09 15:07:56 +0530

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const jwt = require('jsonwebtoken')
 const userController = require('../controllers/userController');
 const { roles } = require('../roles');
-router.get('/restaurants', userController.allowIfLoggedin , userController.getRestaurants)
-router.get('/users', userController.allowIfLoggedin, userController.getUsers)
-router.get('/getSubAdmins', userController.allowIfLoggedin, userController.getSubAdmins)
-router.get('/getDishes', userController.allowIfLoggedin, userController.getDishes)
-router.get('/logout', userController.allowIfLoggedin, userController.logout)
-router.post('/createUser', userController.createUser)
-router.post('/createRestaurants',  userController.allowIfLoggedin, userController.createRestaurants)
-//router.post('/createDishes',userController.allowIfLoggedin, userController.createDishes)
-router.post('/createSubAdmins',userController.allowIfLoggedin, userController.createSubAdmins)
-router.post('/login',  userController.login)
-router.post('/signup', userController.signup)
+router.get('/restaurants', userController.allowIfLoggedin , userController.getRestaurants) //API to fetch a list of all restaurants
+router.get('/users', userController.allowIfLoggedin, userController.getUsers)  //API for admin/sub-admin to list all users
+router.get('/getSubAdmins', userController.allowIfLoggedin, userController.getSubAdmins)  //API for admin to list all sub-admins
+router.get('/getDishes', userController.allowIfLoggedin, userController.getDishes)  //API to fetch dishes for a given restaurant
+router.post('/createUser', userController.createUser)  //API for admin/sub-admin to create user
+router.post('/createRestaurants',  userController.allowIfLoggedin, userController.createRestaurants)  //API for admin/sub-admin to create Restaurants
+router.post('/createSubAdmins',userController.allowIfLoggedin, userController.createSubAdmins)  //API for admin/sub-admin to create sub admins
+router.post('/login',  userController.login)  //API for login
+router.post('/signup', userController.signup)  // API for signup
+router.get('/logout', userController.allowIfLoggedin, userController.logout)  //API for logout
+
 
 
 //router.put("/logout",userController.allowIfLoggedin,userController.logout);
